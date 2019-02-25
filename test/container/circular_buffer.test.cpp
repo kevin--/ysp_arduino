3bb98179ab833b55641b89d03ca04ec17f2a567f
Kevin Dixon <kevin.c.dixon@gmail.com>
2019-02-24 20:01:44 -0800

    tests for circular buffer done

--- test/container/circular_buffer.test.cpp
+++ test/container/circular_buffer.test.cpp
@@ -17,6 +17,57 @@ TEST( ciruclar_buffer_test, initial_state )
 
     EXPECT_EQ( buffer.size(), 0 );
     EXPECT_EQ( buffer.capacity(), capacity );
+    EXPECT_EQ( buffer.available(), capacity );
     EXPECT_TRUE( buffer.canWrite() );
     EXPECT_FALSE( buffer.canRead() );
+}
+
+TEST( ciruclar_buffer_test, write_api_works )
+{
+    constexpr size_t                          capacity = 8;
+    ysp::circular_buffer< int16_t, capacity > buffer;
+
+    for ( int16_t i = 0; i < static_cast< int16_t >( buffer.capacity() ); ++i )
+    {
+        EXPECT_TRUE( buffer.canWrite() );
+        buffer.write( i );
+        EXPECT_TRUE( buffer.canRead() );
+        EXPECT_EQ( buffer.size(), i + 1 );
+        EXPECT_EQ( buffer.available(), buffer.capacity() - i - 1 );
+    }
+
+    EXPECT_TRUE( buffer.canRead() );
+    EXPECT_FALSE( buffer.canWrite() );
+    EXPECT_EQ( buffer.available(), 0 );
+    EXPECT_EQ( buffer.size(), buffer.capacity() );
+}
+
+TEST( ciruclar_buffer_test, read_api_works )
+{
+    constexpr size_t                          capacity = 8;
+    ysp::circular_buffer< int16_t, capacity > buffer;
+
+    for ( int16_t i = 0; i < static_cast< int16_t >( buffer.capacity() ); ++i )
+    {
+        buffer.write( i );
+    }
+    EXPECT_TRUE( buffer.canRead() );
+    EXPECT_FALSE( buffer.canWrite() );
+    EXPECT_EQ( buffer.available(), 0 );
+    EXPECT_EQ( buffer.size(), buffer.capacity() );
+
+    int16_t expected_value   = 0;
+    size_t  capacity_counter = capacity;
+
+    while ( buffer.canRead() )
+    {
+        EXPECT_EQ( buffer.size(), capacity_counter-- );
+        int16_t value = buffer.read();
+        EXPECT_EQ( value, expected_value++ );
+    }
+
+    EXPECT_EQ( buffer.size(), 0 );
+    EXPECT_EQ( buffer.available(), capacity );
+    EXPECT_FALSE( buffer.canRead() );
+    EXPECT_TRUE( buffer.canWrite() );
 }
